2d4ddda2c96cdbe42a115b8787f469e60e5e4c3a
Melissa Ettinger <mettinger@seamgen.com>
2015-09-06 10:48:08 -0700

    Registration code validation

--- client/app/register/registerController.js
+++ client/app/register/registerController.js
@@ -1,23 +1,54 @@
-(function() {
+(function () {
     angular
         .module('coder-dojo-signup')
         .controller('RegisterController', RegisterController);
 
     RegisterController.$inject = [
+        '$http',
         '$scope',
         '$rootScope',
         '$state'
     ];
 
-    function RegisterController($scope, $rootScope, $state) {
+    function RegisterController($http, $scope, $rootScope, $state) {
         var vm = this;
 
         vm.code = '';
 
         vm.submit = submit;
 
-        function submit () {
-            alert(vm.code);
+        function submit() {
+            vm.code = vm.code.trim().toLowerCase();
+            vm.code = vm.code.replace(/\s+/g, '');
+
+            console.log(vm.code);
+
+            var req = {
+                method: 'GET',
+                url: '/api/validateCodes',
+                data: vm.code
+            };
+
+            $http(req)
+                .then(success, error);
+
+            function success(response) {
+                console.log(response);
+
+                $state.go('form');
+            }
+
+            function error(response) {
+                console.log(response);
+
+                if (status === 401) {
+                    alert('Sorry, that code is invalid');
+                }
+                else {
+                    $state.go('error');
+                }
+            }
+
         }
     }
 })();
