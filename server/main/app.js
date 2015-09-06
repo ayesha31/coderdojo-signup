43ceec1a67895e06268b19532d8399980f02ea1c
Melissa Ettinger <mettinger@seamgen.com>
2015-09-06 01:21:16 -0700

    Deployment bug fix

--- server/main/app.js
+++ server/main/app.js
@@ -10,7 +10,7 @@ var db = require('monk')(process.env.DB_URL || 'mongodb://localhost/coder-dojo-s
 var app = express();
 
 
-require('./config.js')(app, express, routers);
+require('./config.js')(app, express);
 
 require('../form/ninjaRegistration.js')(app, db);
 require('../view/getNinjaList.js')(app, db);
