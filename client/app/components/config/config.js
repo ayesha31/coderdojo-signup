4543890131d8ee97791877c40db378be2a2f8e82
Tim Veletta <timothy.veletta@gmail.com>
2016-02-10 09:09:36 +0800

    Closed sign ups.

--- client/app/components/config/config.js
+++ client/app/components/config/config.js
@@ -9,25 +9,25 @@
     ];
 
     function config($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/form');
+        $urlRouterProvider.otherwise('/closed');
 
         $stateProvider
-            .state('form', {
-                url: '/form',
-                templateProvider: function ($templateCache) {
-                    return $templateCache.get('app/form/form.html');
-                },
-                controller: 'FormController',
-                controllerAs: 'vm'
-            })
-            .state('accept', {
-                url: '/accept',
-                templateProvider: function ($templateCache) {
-                    return $templateCache.get('app/accept/accept.html');
-                },
-                controller: 'AcceptController',
-                controllerAs: 'vm'
-            })
+            // .state('form', {
+            //     url: '/form',
+            //     templateProvider: function ($templateCache) {
+            //         return $templateCache.get('app/form/form.html');
+            //     },
+            //     controller: 'FormController',
+            //     controllerAs: 'vm'
+            // })
+            // .state('accept', {
+            //     url: '/accept',
+            //     templateProvider: function ($templateCache) {
+            //         return $templateCache.get('app/accept/accept.html');
+            //     },
+            //     controller: 'AcceptController',
+            //     controllerAs: 'vm'
+            // })
             .state('error', {
                 url: '/error',
                 templateProvider: function ($templateCache) {
@@ -40,12 +40,12 @@
                     return $templateCache.get('app/closed/closed.html');
                 }
             })
-            .state('view', {
-                url: '/view',
-                templateProvider: function ($templateCache) {
-                    return $templateCache.get('app/view/view.html');
-                },
-                controller: 'ViewController'
-            });
+            // .state('view', {
+            //     url: '/view',
+            //     templateProvider: function ($templateCache) {
+            //         return $templateCache.get('app/view/view.html');
+            //     },
+            //     controller: 'ViewController'
+            // });
     }
 })();
